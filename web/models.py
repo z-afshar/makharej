b05da7122fe23f7302d07825937e7a34d94f89c3
Zahra Afshar <mrs.zahra.afshar@gmail.com>
2017-01-22 15:24:38 +0330

    add expenses through post request

--- web/models.py
+++ web/models.py
@@ -3,6 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length = 48)
+    def __unicode__(self):
+        return "{}_token".format(self.user)    
+
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
--- web/views.py
+++ web/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from json import JSONEncoder
+from django.views.decorators.csrf import csrf_exempt
+from web.models import User, Token, Expense, Income
+from datetime import datetime
 # Create your views here.
+
+@csrf_exempt
+def submit_expense(request):
+    """user submits an expense"""
+
+    #TODO; validate data. user & token & amount might be fake
+    this_token = request.POST.get('token','1234')
+    this_user = User.objects.filter(token__token = this_token).get()
+    if 'date' not in request.POST:
+        date = datetime.now()
+    Expense.objects.create(user = this_user, amount = request.POST['amount'],
+        text = request.POST['text'], date=date)
+    print "I'm in submit expense"
+    print request.POST
+
+    return JsonResponse({
+        'status': 'ok',
+    }, encoder=JSONEncoder)
